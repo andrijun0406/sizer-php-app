53cc9e6a684dab72f0da4815cdaf2b7ec6495633
Andri Junardi Ahmad <Andri.Ahmad@dell.com>
2025-09-13 21:50:34 +0700

    change indexx.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // This is a simple PHP script
-echo "<h1>Welcome to My First PHP Page!</h1>";
+echo "<h1>Welcome to Azure Local Sizer App</h1>";
 echo "<p>Today is " . date("l, F jS Y") . "</p>";
 ?>
