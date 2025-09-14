3c8586936be7fc8de8126c9e91156ea15906b18e
Andri Junardi Ahmad <Andri.Ahmad@dell.com>
2025-09-15 05:14:41 +0700

    edit require_once

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-//require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../src/InputHandler.php';
-require_once __DIR__ . '/../src/VMCalculator.php';
-require_once __DIR__ . '/../src/OutputRenderer.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+//require_once __DIR__ . '/../src/InputHandler.php';
+//require_once __DIR__ . '/../src/VMCalculator.php';
+//require_once __DIR__ . '/../src/OutputRenderer.php';
 
 
 use App\InputHandler;
